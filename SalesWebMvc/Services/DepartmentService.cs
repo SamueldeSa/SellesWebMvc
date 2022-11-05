825d7f6d136c01c9d714a52613c4766da1f4cf43
Samuel Vitor <samuelsaoliveira@gmail.com>
2022-11-04 23:23:24 -0300

    Asynchronous operation using Task (async,await)

--- SalesWebMvc/Services/DepartmentService.cs
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
 {
@@ -17,9 +18,9 @@ namespace SalesWebMvc.Services
         }
 
         //Metodo para retornar todos department da list
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
 
     }
